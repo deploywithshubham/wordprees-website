384cabeae950b25ea40c52040d7308a0c2e8c498
Shubham Trivedi <shubham.maskoid@gmail.com>
2022-06-23 01:54:16 +0530

    new files

---
+++ wp-admin/moderation.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>Redirecting...</title>
+		<meta http-equiv="refresh" content="0;url=https://deploywithshubham.github.io/wordprees-website/wp-admin/edit-comments.php?comment_status=moderated">
+	</head>
+	<body>
+		<script type="text/javascript">
+			window.location = "https://deploywithshubham.github.io/wordprees-website/wp-admin/edit-comments.php?comment_status=moderated";
+		</script>
+
+		<p>You are being redirected to <a href="https://deploywithshubham.github.io/wordprees-website/wp-admin/edit-comments.php?comment_status=moderated">https://deploywithshubham.github.io/wordprees-website/wp-admin/edit-comments.php?comment_status=moderated</a></p>
+	</body>
+</html>
---
+++ wp-links-opml.php
@@ -0,0 +1,11 @@
+<?xml version="1.0"?>
+<opml version="1.0">
+	<head>
+		<title>
+		Links for Nexusry		</title>
+		<dateCreated>Wed, 22 Jun 2022 19:57:52 GMT</dateCreated>
+		<!-- generator="WordPress/6.0" -->
+	</head>
+	<body>
+</body>
+</opml>
